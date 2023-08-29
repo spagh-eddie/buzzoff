41c99b6627a8abe4199eefa086d76bc9c733b533
Eddie Darling <darling@berkeley.edu>
2023-08-29 15:54:04 -0700

    Use a better word list
    The previous one did not have plurals, specifically "brawls"

--- src/buzz/common.py
+++ src/buzz/common.py
@@ -1,7 +1,9 @@
 """Common utilities"""
 
+from pathlib import Path
+
 
 def default_words() -> list[str]:
     """Default words if not provided"""
-    with open("/usr/share/dict/words", "r", encoding="utf-8") as f:
-        return tuple(f.read().splitlines())
+    with open(Path(__file__).parent / "words_alpha.txt", "r", encoding="utf-8") as f:
+        return f.read().splitlines()
